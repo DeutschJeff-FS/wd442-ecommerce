771bcc6defe3d0cb56bf94aee99359af034fa088
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2022-05-11 14:38:59 -0400

    feat: add pug template engine

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const productRouter = require("./routes/Products");
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("views", __dirname + "/templates/views");
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.use("/products", productRouter);
 

eb4f15b3267176e09c8ad8985225c2f39635cb76
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2022-05-27 00:14:47 -0400

    initial commit

---
+++ routes/Images.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const imageCtrl = require("../controllers/Images");
+
+router.get("/", imageCtrl.index);
+router.get("/new", imageCtrl.form);
+router.get("/:id", imageCtrl.show);
+router.get("/:id/edit", imageCtrl.form);
+
+router.post("/", imageCtrl.create);
+router.post("/:id", imageCtrl.update);
+router.all("/:id/delete", imageCtrl.remove);
+
+router.delete("/:id", imageCtrl.remove);
+
+module.exports = router;
---
+++ routes/Variants.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const variantCtrl = require("../controllers/Variants");
+
+router.get("/", variantCtrl.index);
+router.get("/new", variantCtrl.form);
+router.get("/:id", variantCtrl.show);
+router.get("/:id/edit", variantCtrl.form);
+
+router.post("/", variantCtrl.create);
+router.post("/:id", variantCtrl.update);
+router.all("/:id/delete", variantCtrl.remove);
+
+router.delete("/:id", variantCtrl.remove);
+
+module.exports = router;
